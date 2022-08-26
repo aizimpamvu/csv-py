c041e04ead3ab2f4a33a82caa998a3b9cbe21752
aizimpamvu <hallyiizza200@gmail.com>
2022-08-26 13:54:06 +0200

    initial commit

--- main.py
+++ main.py
@@ -5,6 +5,10 @@
 import csv
 
 with open("weather_data.csv") as data_file:
-    data=csv.reader(data_file)
+    data = csv.reader(data_file)
+    temperature = []
     for row in data:
-        print(row)
+        # print(row)
+        if row[1] != "temp":
+            temperature.append(row[1])
+    print(temperature)
